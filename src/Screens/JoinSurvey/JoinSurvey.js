a5555f3dab53136acb421b1e8bf02328f844d529
dev-souravsharma <dev.souravsharma@gmail.com>
2021-06-03 17:44:10 +0530

    bugs fixed

--- src/Screens/JoinSurvey/JoinSurvey.js
+++ src/Screens/JoinSurvey/JoinSurvey.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button, FlatList} from 'react-native';
 import Menu from '../../Components/Menu';
 import english from '../../utils/Language/String';
@@ -48,8 +48,11 @@ const JoinSurvey = props => {
   const [toggleCheckBox, setToggleCheckBox] = useState(false);
   const [optionsData, setOptionsData] = useState(data);
   const [flag, setFlag] = useState(false);
+  const [getIndex, setIndex] = useState(-1);
+  const [count, setCount] = useState(0);
   const {title} = props.route.params;
   const dataSize = (100 / (data.length - position + 1)).toFixed(2).toString();
+  console.log(flag);
   return (
     <View style={styles.container}>
       {/* Menu */}
@@ -58,7 +61,13 @@ const JoinSurvey = props => {
         title={title}
         background="#FFA000"
         goBack={() => {
-          props.navigation.goBack();
+          if (position - 1 === 0) {
+            props.navigation.goBack();
+            setFlag(false);
+          } else {
+            changePosition(position - 1);
+            setFlag(true);
+          }
         }}
       />
 
@@ -85,7 +94,7 @@ const JoinSurvey = props => {
         {/* CheckBox  */}
         <FlatList
           data={optionsData[position - 1].options}
-          extraData={toggleCheckBox}
+          extraData={(toggleCheckBox, getIndex)}
           renderItem={({item, index}) => {
             return (
               <View style={styles.checkBoxContainer}>
@@ -96,8 +105,27 @@ const JoinSurvey = props => {
                     setToggleCheckBox(!toggleCheckBox);
                     let list = optionsData;
                     list[position - 1].options[index].selected = newValue;
-                    // console.log(list[position - 1].options[0].selected);
+                    console.log(list[position - 1].options);
                     setOptionsData(list);
+                    setIndex(index);
+                    // Set Logic Here
+                    console.log(index, getIndex);
+                    setFlag(!toggleCheckBox);
+                    list[position - 1].options.forEach(function (val, i, arr) {
+                      if (arr[i].selected === true) {
+                        // console.log('Data Hey');
+                        setFlag(true);
+                      }
+                      // if (arr[i].selected === false) {
+                      //   setCount(count + 1);
+                      // }
+                    });
+                    // console.log(list[position - 1].options.length);
+                    // console.log(count);
+                    // if (list[position - 1].options.length === count) {
+                    //   setFlag(false);
+                    //   alert('Hrllo');
+                    // }
                   }}
                 />
                 <Text>{item.ans}</Text>
@@ -114,6 +142,7 @@ const JoinSurvey = props => {
               <Button
                 onPress={() => {
                   changePosition(position - 1);
+                  setFlag(!flag);
                 }}
                 title="back"
                 color="grey"
@@ -123,8 +152,10 @@ const JoinSurvey = props => {
           {position === data.length || (
             <View>
               <Button
+                disabled={!flag}
                 onPress={() => {
                   changePosition(position + 1);
+                  setFlag(false);
                 }}
                 title={english.saveNext}
                 color="green"
@@ -134,6 +165,7 @@ const JoinSurvey = props => {
           {position === data.length && (
             <View>
               <Button
+                disabled={!flag}
                 onPress={() => {
                   props.navigation.navigate('NewsFeed');
                 }}
